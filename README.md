e92e00caa51b78c8fab65b93bfdbd0ddd397b5b7
Omar Shaban <3marshabanx@gmail.com>
2023-06-30 20:00:59 +0300

    Show alert when somthing wrong

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # excel-sheet-generator
 
-hello there, your task is to use sweetalert.js to display an alert when the user clicks "generate" while the fields are empty, and when the user clicks "export" but there is no generated table to be exported.
+Hello there, It is a task is to use sweetalert.js to display an alert when the user clicks "generate" while the fields are empty, and when the user clicks "export" but there is no generated table to be exported.
 
-fork the repository and start working on it.
-
-best of luck.
+Good bye!
--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 let table = document.getElementsByClassName("sheet-body")[0],
-rows = document.getElementsByClassName("rows")[0],
-columns = document.getElementsByClassName("columns")[0]
-tableExists = false
+  rows = document.getElementsByClassName("rows")[0],
+  columns = document.getElementsByClassName("columns")[0];
+tableExists = false;
 
 const generateTable = () => {
-    let rowsNumber = parseInt(rows.value), columnsNumber = parseInt(columns.value)
-    table.innerHTML = ""
-    for(let i=0; i<rowsNumber; i++){
-        var tableRow = ""
-        for(let j=0; j<columnsNumber; j++){
-            tableRow += `<td contenteditable></td>`
-        }
-        table.innerHTML += tableRow
+  let rowsNumber = parseInt(rows.value),
+    columnsNumber = parseInt(columns.value);
+  table.innerHTML = "";
+  for (let i = 0; i < rowsNumber; i++) {
+    var tableRow = "";
+    for (let j = 0; j < columnsNumber; j++) {
+      tableRow += `<td contenteditable></td>`;
     }
-    if(rowsNumber>0 && columnsNumber>0){
-        tableExists = true
-    }
-}
+    table.innerHTML += tableRow;
+  }
+  if (rowsNumber > 0 && columnsNumber > 0) {
+    tableExists = true;
+  } else {
+    Swal.fire("Oops!", "Select number of rows and columns!", "warning");
+  }
+};
 
 const ExportToExcel = (type, fn, dl) => {
-    if(!tableExists){
-        return
-    }
-    var elt = table
-    var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" })
-    return dl ? XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' })
-        : XLSX.writeFile(wb, fn || ('MyNewSheet.' + (type || 'xlsx')))
-}
+  if (!tableExists) {
+    Swal.fire(
+      "Oops!",
+      "Select number of rows and columns to export!",
+      "warning"
+    );
+    return;
+  }
+  var elt = table;
+  var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
+  return dl
+    ? XLSX.write(wb, { bookType: type, bookSST: true, type: "base64" })
+    : XLSX.writeFile(wb, fn || "MyNewSheet." + (type || "xlsx"));
+};
